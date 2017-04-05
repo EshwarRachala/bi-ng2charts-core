19089a270cb8b56f34ea7186e2cefb7e8cd7797a
eshwar rachala <eshwar.appdev@gmail.com>
2017-04-04 22:03:04 -0400

    updated

--- src/shared.component.ts
+++ src/shared.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'shared-component',
-    template: `<h1>Sample component</h1>`
+    template: `<h1>Shared component</h1>`
 })
 
 export class SharedComponent {
