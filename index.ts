7f117d62416ae6d185a5110b8eebc79969846a98
eshwar rachala <eshwar.appdev@gmail.com>
2017-04-08 02:03:37 -0400

    feat(added bubble chart component):

--- index.ts
+++ index.ts
@@ -3,18 +3,25 @@ import { CommonModule } from '@angular/common';
 import { SharedComponent } from './src/shared.component';
 import { BarChartComponent } from './src/barchart.component';
 
+import { BubbleChartComponent } from './src/bubblechart.component';
+
 export * from './src/shared.component';
 export * from './src/barchart.component';
+export * from './src/bubbles.component';
+export * from './src/bubblechart.component';
+
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
     SharedComponent,
-    BarChartComponent
+    BarChartComponent,
+    BubbleChartComponent
     ],
   exports: [
     SharedComponent,
-    BarChartComponent
+    BarChartComponent,
+    BubbleChartComponent
   ]
 })
 export class SharedModule {
